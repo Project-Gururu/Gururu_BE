51fca0ee439248600984cf66d8e0f06939449917
kiwon kwon <funnykyeon@naver.com>
2022-09-04 18:50:21 +0900

    FEAT:Add Gururu base code

---
+++ src/main/java/com/example/gururu_be/GururuBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gururu_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GururuBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GururuBeApplication.class, args);
+    }
+
+}
