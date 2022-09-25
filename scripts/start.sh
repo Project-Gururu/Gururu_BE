b1f285bd6c69467c2d6d9baaf36a11ddd557f9e3
kiwon kwon <funnykyeon@naver.com>
2022-09-25 16:12:23 +0900

    chore: DB TEST
    단순테스트
    
    funnykyeon

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,5 +12,5 @@ sudo docker build --build-arg IDLE_PROFILE=${IDLE_PROFILE} -f /home/ec2-user/cic
 sleep 30
 
 echo "> 도커 컨테이너를 실행합니다." >> /home/ec2-user/deploy.log
-sudo docker run -e DB_URL=${DB_URL} -e DB_UserNAME=${DB_USERNAME} -e DB_PASSWORD=${DB_PASSWORD} -p ${IDLE_PORT}:${IDLE_PORT} ${IDLE_PROFILE} >> /home/ec2-user/spring.log 2>/home/ec2-user/deploy_err.log &
+sudo docker run -e DB_URL=${DB_URL} -e DB_USERNAME=${DB_USERNAME} -e DB_PASSWORD=${DB_PASSWORD} -p ${IDLE_PORT}:${IDLE_PORT} ${IDLE_PROFILE} >> /home/ec2-user/spring.log 2>/home/ec2-user/deploy_err.log &
 sleep 10
