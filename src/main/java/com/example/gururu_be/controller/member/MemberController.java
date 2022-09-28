d01694e0b41f7e2ef591c4b51107e07bd252cb42
kiwon kwon <funnykyeon@naver.com>
2022-09-28 16:42:20 +0900

    feat: [#16] M1 member, memberLocalization API 구현
    M1-4 | 회원정보수정 - 완료
    M1-5 | 회원 위치 저장 - 완료
    M1-6 | 회원 위치 전체조회
    M1-7 | 회원 위치 수정 - 완료
    M1-8 | 회원 위치 삭제 - 완료
    
    funnykyeon

--- src/main/java/com/example/gururu_be/controller/member/MemberController.java
+++ src/main/java/com/example/gururu_be/controller/member/MemberController.java
@@ -1,14 +1,17 @@
-package com.example.gururu_be.controller;
+package com.example.gururu_be.controller.member;
 
 import com.example.gururu_be.domain.dto.ResResultDto;
 import com.example.gururu_be.domain.dto.member.ReqMemberInfoDto;
 import com.example.gururu_be.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/member")
+@RequestMapping("/user/v1.0/member")
 @RequiredArgsConstructor
 public class MemberController {
 
@@ -17,7 +20,7 @@ public class MemberController {
     /**
      *  M-1-4 회정 정보 수정
      */
-    @PatchMapping("/update")
+    @PutMapping("/")
     public ResponseEntity<ResResultDto> modifyMember(@RequestBody ReqMemberInfoDto reqMemberInfoDto) {
 
         memberService.updateMemberInfo(reqMemberInfoDto);
@@ -25,4 +28,7 @@ public class MemberController {
         // 반환값 : 결과 메시지, 상태값(200)
         return ResponseEntity.ok(new ResResultDto("회원 정보 수정 성공"));
     }
+
+
+
 }
