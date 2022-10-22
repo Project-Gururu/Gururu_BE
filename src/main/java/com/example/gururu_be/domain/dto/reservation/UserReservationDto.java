4fbb7aca9afe4286706c9e2b33fd20a7e08508a3
kiwon kwon <funnykyeon@naver.com>
2022-10-22 15:57:59 +0900

    feat:[#44] M4_Reservation 기능 일부 구현
    M4-1 | 예약 등록 구현완료
    M4-2 | 예약 취소 구현완료(유저)
    M4-3 | 예약 상세 조회(유저) 구현완료
    M4-4 | 예약 전체 조회(유저) 구현완료
    M4-7 | 예약 수락/거부(사업자) 구현완료
    
    funnykyeon

---
+++ src/main/java/com/example/gururu_be/domain/dto/reservation/UserReservationDto.java
@@ -0,0 +1,58 @@
+package com.example.gururu_be.domain.dto.reservation;
+
+import com.example.gururu_be.domain.entity.reservation.Reservation;
+import com.example.gururu_be.enumerate.RefuseState;
+import com.example.gururu_be.enumerate.ReservationState;
+import com.example.gururu_be.enumerate.ReviewState;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserReservationDto {
+
+    private UUID reservationId;
+
+    private UUID mbId;
+
+    private UUID petId;
+
+    private UUID storeRegisterId;
+
+    private UUID productId;
+
+    private UUID beauticianId;
+
+    private ReservationState reservationState;
+
+    private String reservationTime;
+
+    private String requestsInfo;
+
+    private RefuseState refuseState;
+
+    private String refuseMessage;
+
+    private ReviewState reviewState;
+
+    public UserReservationDto(Reservation reservation) {
+        this.reservationId = reservation.getId();
+        this.mbId = reservation.getMember().getId();
+        this.petId = reservation.getPet().getId();
+        this.storeRegisterId = reservation.getStore().getId();
+        this.productId = reservation.getProduct().getId();
+        this.beauticianId = reservation.getBeautician().getId();
+        this.reservationState = reservation.getReservationState();
+        this.reservationTime = reservation.getReservationTime();
+        this.requestsInfo = reservation.getRequestsInfo();
+        this.refuseState = reservation.getRefuseState();
+        this.refuseMessage = reservation.getRefuseMessage();
+        this.reviewState = reservation.getReviewState();
+    }
+}
---
+++ src/main/java/com/example/gururu_be/enumerate/RefuseState.java
@@ -0,0 +1,5 @@
+package com.example.gururu_be.enumerate;
+
+public enum RefuseState {
+    YES_RESERVATION, NO_RESERVATION, REFUSE_WAITING
+}
